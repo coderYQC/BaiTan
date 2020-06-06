fa3976798cd37806f46dea9b88cd19be872affca
yqc5521 <595672928@qq.com>
2020-06-06 12:52:06 +0800

    Initial Commit

---
+++ BaiTan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BaiTan
+//
+//  Created by yanqunchao on 2020/6/6.
+//  Copyright © 2020 yanqunchao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
